fcb151bd261c10756cdb137ee086c1e3aa6f7557
i314q159 <i314q159@outlook.com>
2026-03-28 00:13:23 +0800

    Update a.c

--- src/a.c
+++ src/a.c
@@ -2,8 +2,9 @@
 
 #include <stdio.h>
 #include <iso646.h>
-void a() 
-{    
-    printf( "%i\n" , 0 and 1);
-    printf( "%i\n" , 0 or 1);
+
+void a()
+{
+    printf("%i\n", 0 and 1);
+    printf("%i\n", 0 or 1);
 }
