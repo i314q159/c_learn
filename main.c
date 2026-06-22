d9ac8e323fcd661ef4f3d773320cb1041e2bed7c
i314q159 <i314q159@outlook.com>
2026-06-22 19:50:22 +0800

    update

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include "a.h"
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
     a();
     return 0;
 }
--- src/a.c
+++ src/a.c
@@ -1,10 +1,9 @@
 #include "a.h"
 
-#include <stdio.h>
 #include <iso646.h>
+#include <stdio.h>
 
-void a()
-{
+void a() {
     printf("%i\n", 0 and 1);
     printf("%i\n", 0 or 1);
 }
